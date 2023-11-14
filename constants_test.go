92e78812878f0a13b6c1c4ebbeb58d2969697191
Oleksandr Tkachuk <tksasha.me@gmail.com>
2023-11-14 16:29:21 +0200

    added CANT_BE_BLANK validation error message

---
+++ constants_test.go
@@ -0,0 +1,15 @@
+package validations
+
+import (
+	"testing"
+)
+
+func TestCantBeBlankMessage(t *testing.T) {
+	subject := CANT_BE_BLANK
+
+	expected := "can't be blank"
+
+	if subject != expected {
+		t.Errorf(message, subject, expected)
+	}
+}
